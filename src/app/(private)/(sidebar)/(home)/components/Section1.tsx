6e9e75f66e7636fad2715177cfefa8546d789032
maycon600 <mayconvasconcellos2010@hotmail.com>
2025-07-03 10:02:28 -0400

    refactor(feature): home header cards and responsivity

--- src/app/(private)/(sidebar)/(home)/components/Section1.tsx
+++ src/app/(private)/(sidebar)/(home)/components/Section1.tsx
@@ -6,7 +6,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ApexOptions } from "apexcharts";
-import { ChevronDown, Info } from "lucide-react";
+import { ChevronDown, ChevronRight, Info } from "lucide-react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
@@ -117,42 +117,67 @@ export function Section1() {
           <span className="text-dark font-semibold">Dados de Políticos</span>
           <Info className="text-light-dark" />
         </div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <div className="text-secondary border-secondary flex items-center justify-between rounded-xl border-2 px-2 py-1 font-semibold">
-              <span>Trocar Político</span>
-              <ChevronDown />
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>Lorem Ipsum</DropdownMenuItem>
-            <DropdownMenuItem>Lorem Ipsum</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="hidden md:block">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <div className="text-secondary border-secondary flex items-center justify-between rounded-xl border-2 px-2 py-1 font-semibold">
+                <span>Trocar Político</span>
+                <ChevronDown />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Lorem Ipsum</DropdownMenuItem>
+              <DropdownMenuItem>Lorem Ipsum</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
-      <div className="flex w-full flex-col items-center gap-2 xl:flex-row xl:gap-4">
-        <Image
-          src="/static/politician.png"
-          alt=""
-          width={500}
-          height={1000}
-          className="h-40 w-full rounded-xl object-contain lg:w-28 xl:h-full xl:w-40 xl:object-cover"
-        />
-        <div className="border-secondary flex h-full w-full flex-col rounded-xl border-2 p-1 xl:w-1/2">
+      <div className="grid w-full grid-cols-1 items-center gap-4 px-2 md:grid-cols-11 md:px-0 xl:gap-4">
+        <div className="flex h-full w-full flex-row gap-8 lg:col-span-2">
+          <Image
+            src="/static/politician2.png"
+            alt=""
+            width={500}
+            height={1000}
+            className="w-1/2 rounded-xl object-contain lg:h-full lg:w-full xl:object-cover"
+          />
+          <div className="flex h-full flex-1 flex-col justify-between rounded-lg md:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <div className="text-secondary border-secondary flex items-center justify-between rounded-xl border-2 px-2 py-1 font-semibold">
+                  <span>Trocar Político</span>
+                  <ChevronDown />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>Lorem Ipsum</DropdownMenuItem>
+                <DropdownMenuItem>Lorem Ipsum</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <div className="bg-primary/20 flex w-full flex-row items-center justify-center gap-2 rounded-lg p-2">
+              <span className="text-primary text-lg">Partido:</span>
+              <span className="text-primary text-lg">PL</span>
+            </div>
+            <div className="bg-primary/20 flex w-full flex-col items-center justify-center gap-2 rounded-lg p-2">
+              <span className="text-primary text-lg">Samuel Viana</span>
+            </div>
+          </div>
+        </div>
+        <div className="border-secondary flex h-full w-full flex-col rounded-xl border-2 p-1 lg:col-span-5">
           <div className="flex w-full justify-between">
             <div className="flex flex-col">
-              <span className="font-semibold">GASTOS E RECURSOS</span>
+              <span className="text-secondary font-bold">
+                GASTOS E RECURSOS
+              </span>
               <div className="flex items-center gap-1">
                 <span className="text-sm">Custos de Maio:</span>
                 <span className="font-semibold text-red-500">R$40.688,29</span>
               </div>
             </div>
-            <div className="bg-secondary/40 flex h-8 w-40 items-center justify-between gap-2 rounded-md px-1 text-sm text-white">
+            <div className="flex h-8 items-center justify-between gap-2 rounded-md px-1 text-sm text-white">
               <span className="border-secondary bg-secondary cursor-pointer rounded-md border p-0.5 font-semibold">
-                Mês
+                Filtro
               </span>
-              <span className="cursor-pointer font-semibold">2023</span>
-              <span className="cursor-pointer font-semibold">2024</span>
             </div>
           </div>
           <ReactApexChart
@@ -162,59 +187,54 @@ export function Section1() {
             width="100%"
           />
         </div>
-        <div className="bg-secondary flex h-full w-full flex-col rounded-xl p-1 text-white xl:w-1/4">
-          <span className="text-lg font-semibold">CUSTOS DETALHADOS</span>
-          <div className="grid w-full grid-cols-2">
-            <div className="flex items-center border-r border-b border-r-white border-b-white p-1 font-semibold">
-              Pessoal do Gabinete
-            </div>
-            <div className="flex gap-1 border-r border-b border-r-white border-b-white p-1">
-              <span className="text-sm font-bold">22</span>
-              <span className="text-xs">
-                pessoas neste ano, sendo 15 ativas atualmente
-              </span>
-            </div>
-
-            <div className="flex items-center border-r border-b border-r-white border-b-white p-1 font-semibold">
-              Salário mensal bruto
-            </div>
-            <div className="flex items-center gap-1 border-r border-b border-r-white border-b-white p-1">
-              <span className="text-sm font-bold">44k</span>
-              <span className="text-xs">R$44.008,52</span>
-            </div>
-
-            <div className="flex items-center border-r border-b border-r-white border-b-white p-1 font-semibold">
-              Imóvel funcional
-            </div>
-            <div className="flex gap-1 border-r border-b border-r-white border-b-white p-1">
-              <span className="text-xs">NÃO FAZ USO</span>
-            </div>
-
-            <div className="flex items-center border-r border-b border-r-white border-b-white p-1 font-semibold">
-              Auxílio-moradia
-            </div>
-            <div className="flex flex-col items-center gap-1 border-r border-b border-r-white border-b-white p-1">
-              <span className="h-max text-sm font-bold">RECEBEU</span>
-              <span className="h-max text-xs">R$51.036,00</span>
-            </div>
-
-            <div className="flex items-center border-r border-b border-r-white border-b-white p-1 font-semibold">
-              Viagens em missão oficial
-            </div>
-            <div className="flex items-center justify-center gap-1 border-r border-b border-r-white border-b-white p-1">
-              <span className="text-xs">VIAGENS</span>
-            </div>
-
-            <div className="flex items-center border-r border-b border-r-white border-b-white p-1 font-semibold">
-              Passaporte diplomático
-            </div>
-            <div className="flex flex-col gap-1 border-r border-b border-r-white border-b-white p-1">
-              <span className="h-max text-sm font-bold">POSSUI</span>
-              <span className="h-max text-xs">PASSAPORTE DIPLOMÁTICO</span>
+        <div className="border-secondary text-primary flex h-full w-full flex-col rounded-xl border p-2 lg:col-span-4">
+          <div className="flex w-full flex-row items-center justify-between">
+            <span className="text-lg font-bold">CUSTOS DETALHADOS</span>
+            <div className="bg-primary flex items-center justify-center rounded-lg p-1 text-sm text-white">
+              Detalhes <ChevronRight />
             </div>
           </div>
+          <div className="flex w-full flex-1 flex-col justify-between gap-2">
+            <div className="border-b-primary flex w-full flex-row items-center justify-between border-b p-2">
+              <div className="flex items-center border-r border-b border-r-white border-b-white px-1 font-semibold">
+                Pessoal do Gabinete
+              </div>
+              <span className="text-center text-sm font-bold">
+                Até 22 pessoas
+              </span>
+            </div>
+            <div className="border-b-primary flex w-full flex-row items-center justify-between border-b p-2">
+              <div className="flex items-center border-r border-b border-r-white border-b-white px-1 font-semibold">
+                Salário mensal bruto
+              </div>
+              <span className="text-center text-xs font-bold">R$44.008,52</span>
+            </div>
+            <div className="border-b-primary flex w-full flex-row items-center justify-between border-b p-2">
+              <div className="flex items-center border-r border-b border-r-white border-b-white px-1 font-semibold">
+                Imóvel funcional
+              </div>
+              <span className="text-xs font-bold">x</span>
+            </div>
+            <div className="border-b-primary flex w-full flex-row items-center justify-between border-b p-2">
+              <div className="flex items-center border-r border-b border-r-white border-b-white px-1 font-semibold">
+                Auxílio-moradia
+              </div>
+              <span className="h-max text-xs font-bold">R$51.036,00</span>
+            </div>
+            <div className="border-b-primary flex w-full flex-row items-center justify-between border-b p-2">
+              <div className="flex items-center border-r border-b border-r-white border-b-white px-1 font-semibold">
+                Viagens em missão oficial
+              </div>
+              <span className="text-xs font-bold">x</span>
+            </div>
+            <div className="border-b-primary flex w-full flex-row items-center justify-between border-b p-2">
+              <div className="flex items-center border-r border-b border-r-white border-b-white px-1 font-semibold">
+                Passaporte diplomático
+              </div>
+              <span className="h-max text-sm font-bold">POSSUI</span>
+            </div>{" "}
+          </div>
         </div>
-        <div className="bg-secondary/20 border-secondary h-full flex-1 rounded-xl border" />
       </div>
     </div>
   );
