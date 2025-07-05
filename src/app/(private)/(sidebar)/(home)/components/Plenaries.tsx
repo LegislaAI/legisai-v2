e6c3b5725b5b0c53084f35902c6d30a498721f47
maycon600 <mayconvasconcellos2010@hotmail.com>
2025-07-05 10:23:39 -0400

    feat(feature): home events

--- src/app/(private)/(sidebar)/(home)/components/Plenaries.tsx
+++ src/app/(private)/(sidebar)/(home)/components/Plenaries.tsx
@@ -11,9 +11,47 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useApiContext } from "@/context/ApiContext";
+import { usePoliticianContext } from "@/context/PoliticianContext";
+import moment from "moment";
+import { useEffect, useState } from "react";
+interface Event {
+  createdAt: string;
+  departmentId: string;
+  description: string;
+  endDate: string | null;
+  eventTypeId: string;
+  id: string;
+  local: string;
+  situation: string;
+  startDate: string;
+  updatedAt: string;
+  uri: string;
+  videoUrl: string | null;
+}
 export function Plenaries() {
   const router = useRouter();
-
+  const { GetAPI } = useApiContext();
+  const [events, setEvents] = useState<Event[]>([]);
+  const { selectedPoliticianId } = usePoliticianContext();
+  async function handleGetEvent() {
+    const response = await GetAPI(
+      `/event?page=1&politicianId=${selectedPoliticianId}`,
+      true,
+    );
+    console.log("response", response.body.events);
+    try {
+      if (response.status === 200) {
+        setEvents(response.body.events);
+        // return response.body.politician;
+      }
+    } catch (error) {
+      console.error("Error carregando politician:", error);
+    }
+  }
+  useEffect(() => {
+    handleGetEvent();
+  }, [selectedPoliticianId]);
   return (
     <div className="flex h-96 w-full flex-col rounded-lg bg-white p-4 lg:w-1/2">
       <div className="flex items-center justify-between">
@@ -46,38 +84,46 @@ export function Plenaries() {
           <ChevronRight className="text-secondary" />
         </button>
       </div>
-      <ScrollArea>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <div
-            onClick={() => router.push("/plenary/1")}
-            key={index}
-            className="group flex h-16 w-full items-center justify-between rounded-lg border border-transparent px-4 py-2 transition duration-200 hover:cursor-pointer hover:border-zinc-200 hover:shadow-sm"
-          >
-            <div className="flex items-center gap-2">
-              <Image
-                src="/logos/small-logo.png"
-                alt=""
-                width={500}
-                height={500}
-                className="h-8 w-8 object-contain"
-              />
-              <div className="flex flex-col">
-                <span className="font-semibold">
-                  Plenário - Sessão Deliberativa
-                </span>
-                <span className="text-secondary">
-                  {new Date().toLocaleDateString("pt-BR")} às{" "}
-                  {new Date().toLocaleTimeString("pt-BR", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </span>
+      {events && events.length > 0 ? (
+        <ScrollArea>
+          {events.map((event, index) => (
+            <div
+              onClick={() => router.push("/plenary/1")}
+              key={index}
+              className="group flex w-full items-center justify-between rounded-lg border border-transparent px-4 py-2 transition duration-200 hover:cursor-pointer hover:border-zinc-200 hover:shadow-sm"
+            >
+              <div className="flex items-center gap-2">
+                <Image
+                  src="/logos/small-logo.png"
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="h-8 w-8 object-contain"
+                />
+                <div className="flex flex-col">
+                  <span className="font-semibold">
+                    Plenário - Sessão Deliberativa
+                    <br />
+                    <span className="line-clamp-2 text-sm overflow-ellipsis text-zinc-600">
+                      {event.description}
+                    </span>
+                  </span>
+                  <span className="text-secondary">
+                    {moment(event.startDate).format("DD/MM/YYYY HH:mm")}
+                  </span>
+                </div>
               </div>
+              <ChevronRight className="fill-secondary text-secondary" />
             </div>
-            <ChevronRight className="fill-secondary text-secondary" />
-          </div>
-        ))}
-      </ScrollArea>
+          ))}
+        </ScrollArea>
+      ) : (
+        <div className="flex flex-1 items-center justify-center">
+          <span className="text-zinc-500">
+            Nenhum plenário encontrado sobre esse político
+          </span>
+        </div>
+      )}
     </div>
   );
 }
