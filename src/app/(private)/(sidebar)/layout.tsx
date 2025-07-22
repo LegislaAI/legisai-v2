8da3af960c31f9b10c67acb252d0dca50f89b8cc
Gabriel <85517869+GabrielFernandes123@users.noreply.github.com>
2025-07-22 13:36:11 -0400

    fix(setup): remove nprogress

--- src/app/(private)/(sidebar)/layout.tsx
+++ src/app/(private)/(sidebar)/layout.tsx
@@ -1,4 +1,3 @@
-import { NavigationProgress } from "@/components/GlobalLoading";
 import { Header } from "@/components/Header";
 import { MobileSidebar } from "@/components/MobileSidebar";
 import { PoliticianContextProvider } from "@/context/PoliticianContext";
@@ -16,7 +15,6 @@ export default function RootLayout({
         <MobileSidebar />
         <div className="flex w-full flex-col p-2 pt-0 xl:p-8">
           <Header />
-          <NavigationProgress />
           {children}
         </div>
       </PoliticianContextProvider>
